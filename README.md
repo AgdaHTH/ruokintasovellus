9733a8e28556f5145e5acdaf9c01fb8217e36c82
Hilla Halla-aho <hilla.halla-aho@helsinki.fi>
2020-01-31 10:41:12 +0200

    Added functionality for showing data for one animal

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 Eläintenruokinnassa (tai vastaavassa) käytettävä sovellus, jossa omistajat (tai muut vastuuhenkilöt) 
 voivat päivittää oman huollettavansa tietoja ja käyttäjät (ruokkijat/huoltajat) lukea niitä.
 
-1. Sovellus Herokussa: https://dashboard.heroku.com/apps/ruokintasovellus (testitunnus: ###, salasana ###)
-2. Käyttötapaukset: https://github.com/AgdaHTH/ruokintasovellus/blob/master/documentation/userstories.md
-3. Tietokantakaavio: https://github.com/AgdaHTH/ruokintasovellus/blob/master/documentation/tietokantakaavio.pdf
+* Sovellus Herokussa: https://dashboard.heroku.com/apps/ruokintasovellus (testitunnus: mina, salasana hei)
+* Käyttötapaukset: https://github.com/AgdaHTH/ruokintasovellus/blob/master/documentation/userstories.md
+* Tietokantakaavio: https://github.com/AgdaHTH/ruokintasovellus/blob/master/documentation/tietokantakaavio.pdf
--- application/tasks/views.py
+++ application/tasks/views.py
@@ -3,22 +3,23 @@ from flask import redirect, render_template, request, url_for
 from flask_login import login_required, current_user
 
 from application.tasks.models import Animal
+from application.auth.models import User
 from application.tasks.forms import AnimalForm
 
 @app.route("/tasks/", methods=["GET"])
 def tasks_index():
     return render_template("tasks/list.html", animals = Animal.query.all())
 
+
+@app.route("/tasks/<animal_id>/", methods=["GET"]) 
+def tasks_show_animal(animal_id):    
+    return render_template("tasks/show.html", animal = Animal.query.get(animal_id))
+
+
 @app.route("/tasks/new/")
 @login_required
 def tasks_form():
     return render_template("tasks/new.html", form = AnimalForm())
-
-@app.route("/tasks/show/", methods=["GET"]) 
-@login_required
-def tasks_show_animal():
-    
-    return render_template("tasks/show.html", animal = Animal.query.filter())
 
 @app.route("/tasks/", methods=["POST"])
 @login_required
